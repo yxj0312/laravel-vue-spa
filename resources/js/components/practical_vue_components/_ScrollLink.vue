8d3dd839abaaf1b34fc0a5bf50d1d954030d97c9
yxj0312 <yanxiajun@hotmail.com>
2019-06-04 21:40:28 +0200

    Add smoothscroll.polyfill()

--- resources/js/components/practical_vue_components/_ScrollLink.vue
+++ resources/js/components/practical_vue_components/_ScrollLink.vue
@@ -6,12 +6,14 @@
 </template>
 
 <script>
+    import smoothscroll from 'smoothscroll-polyfill';
     export default {
         props: ['href'],
         methods: {
             scroll() {
                 // For older browsers, consider pulling in a polyfill.
                 // https://www.npmjs.com/package/smoothscroll-polyfill
+                smoothscroll.polyfill();
                 document.querySelector(this.href)
                         .scrollIntoView({ behavior: 'smooth' });
             }
