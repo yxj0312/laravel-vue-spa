95d4fca409e6231c917e6130ee2289060e699bfd
yxj0312 <yanxiajun@hotmail.com>
2019-07-22 22:06:55 +0200

    Ep 21 10:00

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
-
+import Vuex from 'vuex';
 import axios from 'axios';
 
 import Modal from './plugins/modal/ModalPlugin';
@@ -10,7 +10,21 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 Vue.use(VueRouter);
-Vue.use(Modal);    
+Vue.use(Vuex)
+Vue.use(Modal);
+
+window.store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+
+    mutations: {
+        increment(state) {
+            state.count++;
+        }
+    }
+
+});
 
 let app = new Vue({
     el: '#app',
--- resources/js/components/whatcha_working_on/_Counter.vue
+++ resources/js/components/whatcha_working_on/_Counter.vue
@@ -1,27 +1,26 @@
 <template>
-    <div>
-        {{ count }}
+    <div class="bg-grey w-64 h-64 flex items-center justify-center">
+        <div>
+            {{ count }}
+
+            <p>
+                <button class="bg-blue py-2 px-4 rounded-lg text-white hover:bg-blue-600" @click="increment">+1</button>
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        props: [],
-
-        components: {},
-
-        data() {
-            return {
-                count: 0
+        computed: {
+            count() {
+                return window.store.state.count;
             }
         },
-
-        computed: {
-            
-        },
-
         methods: {
-            
+            increment() {
+                window.store.commit('increment');
+            }
         }
     }
 </script>
