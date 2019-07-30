00a5cdd2a3840d5f05a749fec3e888c59d141a3e
yxj0312 <yanxiajun@hotmail.com>
2019-07-30 21:19:02 +0200

    Change text to body

--- public/js/app.js
+++ public/js/app.js
@@ -3525,11 +3525,11 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
     // }
     // Action version
     addTodo: function addTodo(e) {
-      var text = e.target.value; // trim() 方法会从一个字符串的两端删除空白字符。
+      var body = e.target.value; // trim() 方法会从一个字符串的两端删除空白字符。
       // 在这个上下文中的空白字符是所有的空白字符 (space, tab, no-break space 等) 以及所有行终止符字符（如 LF，CR）
 
-      if (text.trim()) {
-        this.$store.dispatch('addTodo', text);
+      if (body.trim()) {
+        this.$store.dispatch('addTodo', body);
       }
 
       e.target.value = '';
--- resources/js/components/vuextodo/Ep23.vue
+++ resources/js/components/vuextodo/Ep23.vue
@@ -60,11 +60,11 @@
 
             // Action version
             addTodo (e) {
-                const text = e.target.value
+                const body = e.target.value
                 // trim() 方法会从一个字符串的两端删除空白字符。
                 // 在这个上下文中的空白字符是所有的空白字符 (space, tab, no-break space 等) 以及所有行终止符字符（如 LF，CR）
-                if (text.trim()) {
-                    this.$store.dispatch('addTodo', text)
+                if (body.trim()) {
+                    this.$store.dispatch('addTodo', body)
                 }
 
                 e.target.value = ''
