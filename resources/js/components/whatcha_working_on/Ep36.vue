083bbd0d60571de1d0d941ede1df0b5cac3d3720
yxj0312 <yanxiajun@hotmail.com>
2019-08-13 22:49:25 +0200

    Ep 36 Frontend Authorization Brainstorming 03:23

---
+++ resources/js/components/whatcha_working_on/Ep36.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <article>
+            <h1 class="font-bold mb-4">title</h1>
+            <div class="mb-6">body</div>
+        </article>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [],
+
+        components: {},
+
+        data() {
+            return {
+                
+            }
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            
+        }
+    }
+</script>
